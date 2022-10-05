0307125fe78f4559f8654412bd3fac0c5ade663b
Mehmet Can Aygün <mehmetcanaygun@gmail.com>
2022-10-05 16:18:05 +0300

    Add Raleway font & FontAwesome CDN

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      Raleway: ["Raleway, sans-serif"],
+    },
+    container: {
+      padding: "2rem",
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+    },
   },
   plugins: [],
 };
